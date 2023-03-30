0b198d6d73e0ac9ece5b4914b0ac7725827b0a1c
ReyAdr <adrraj3011@icloud.com>
2023-03-30 12:57:30 +0200

    Added RankedGradeBook class and GetLetterGrade method.

---
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace GradeBook.GradeBooks
+{
+    public class RankedGradeBook : BaseGradeBook
+    {
+        public RankedGradeBook(string name): base(name)
+        {
+            Type = Enums.GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (this.Students.Count < 5)
+            {
+                throw new InvalidOperationException();
+            }
+            if (averageGrade >= 80)
+            {
+                return 'A';
+            }
+            else if (averageGrade >= 60)
+            {
+                return 'B';
+            }
+            else if (averageGrade >= 40)
+            {
+                return 'C';
+            }
+            else if (averageGrade >= 20)
+            {
+                return 'D';
+            }
+            else
+            {
+                return 'F';
+            }
+        }
+
+    }
+}
--- GradeBook/GradeBooks/StandardGradeBook.cs
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -6,6 +6,9 @@ namespace GradeBook.GradeBooks
 {
     public class StandardGradeBook : BaseGradeBook
     {
-        public StandardGradeBook(string name) : base(name) { }
+        public StandardGradeBook(string name) : base(name) 
+        {
+            Type = Enums.GradeBookType.Standard;
+        }
     }
 }
